4ad3e0ba83dc0ecd046277ee065b018292e5ba9a
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-26 17:19:29 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('testing');
+});
+
+app.listen(3000, () => {
+  console.log('running on port 3000');
+});
