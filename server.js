e77976a26c5b924c056d70fddd651ef117d8a3f1
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-31 19:02:35 -0500

    Fix heroku db user registration

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-sinuous-28068',
-    user: 'postgres',
-    password: 'test',
-    database: 'face-recognition',
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
