452242987707d3a4e14d7c5804929026cc8a92c3
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-11-21 13:45:35 -0600

    Add morgan for logging and dockerfile

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -19,6 +20,7 @@ const db = knex({
 
 const app = express();
 
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
